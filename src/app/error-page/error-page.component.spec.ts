bb7d46355236a40ae4bd3629b4c817c3084c4059
MarouaniHamza <hamza.marouenihamza@gmail.com>
2023-12-22 11:24:09 +0100

    feat(dev): Fix unit tests

--- src/app/error-page/error-page.component.spec.ts
+++ src/app/error-page/error-page.component.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ErrorPageComponent } from './error-page.component';
+import { AppRoutingModule } from '../routing/app-routing.module';
+import { ActivatedRoute } from '@angular/router';
 
 describe('ErrorPageComponent', () => {
   let component: ErrorPageComponent;
@@ -9,10 +11,13 @@ describe('ErrorPageComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ErrorPageComponent],
+      imports: [AppRoutingModule],
     }).compileComponents();
 
     fixture = TestBed.createComponent(ErrorPageComponent);
+    fixture.debugElement.injector.get(ActivatedRoute);
     component = fixture.componentInstance;
+
     fixture.detectChanges();
   });
 
